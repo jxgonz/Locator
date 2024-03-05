f309ec51dc57ab026332ae895741d0245440d8ad
Johan Gonzalez <johangonzalez7373@gmail.com>
2024-03-05 15:23:45 -0800

    manage appointments

--- Locator/booking/urls.py
+++ Locator/booking/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import bookAppointment
+from .views import bookAppointment, EditAppointmentView, CancelAppointmentView, ManageAppointmentView
 
 app_name = 'appointments'
 
 urlpatterns = [
-    path('book/<str:service_id>/', bookAppointment, name="book")
+    path('book/<str:service_id>/', bookAppointment, name="book"),
+    path('appointments', ManageAppointmentView, name="manage_appointments"),
+    path('appointment/<str:appointment_id>/edit/', EditAppointmentView, name="edit_appointment"),
+    path('appointment/<str:appointment_id>/cancel/', CancelAppointmentView, name="cancel_appointment"),
 ]
--- Locator/booking/views.py
+++ Locator/booking/views.py
@@ -3,6 +3,7 @@ from .models import Appointment
 from posting_service.models import Service
 from .forms import AppointmentForm
 from django.contrib.auth.decorators import login_required
+from .models import Appointment
 
 
 # Create your views here.
@@ -21,3 +22,26 @@ def bookAppointment(request, service_id):
   else:
     form = AppointmentForm()
   return render(request, 'appointment.html', {'form': form, 'service': service})
+
+def EditAppointmentView(request, appointment_id):
+  appointment = Appointment.objects.get(AppointmentID=appointment_id)
+  if request.method == "POST":
+    form = AppointmentForm(request.POST, instance=appointment)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  else:
+    form = AppointmentForm(instance=appointment)
+    
+  return render(request, 'edit_appointment.html', {'form' : form})
+
+def CancelAppointmentView(request, appointment_id):
+  appointment = Appointment.objects.get(AppointmentID=appointment_id)
+  appointment.delete()
+  return redirect('home')
+
+@login_required
+def ManageAppointmentView(request):
+  appointments = Appointment.objects.filter(client=request.user)
+  return render(request, 'manage_appointments.html', {'appointments': appointments})
+  
