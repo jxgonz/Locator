2d240060b945b1df489b1e4069c135a54e18e33a
Marteen122 <156379165+Marteen122@users.noreply.github.com>
2024-03-05 13:02:30 -0800

    Delete buyer.cpp

--- buyer.cpp
+++
@@ -1,9 +0,0 @@
-class Buyer(User):
-    def __init__(self):
-        self.Appointment = []
-    
-    def addAppointment(self,appointment):
-        self.Appointment.append(appointment)
-        
-    
-    
