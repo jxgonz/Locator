4803c5e3a20b26083bc811fade7b21d063c9fea4
qibowen-99 <1700011744@pku.edu.cn>
2024-03-06 12:16:21 -0800

    test cases for creating accounts

--- Locator/login/tests.py
+++ Locator/login/tests.py
@@ -3,7 +3,7 @@
 from django.urls import reverse
 from django.test import TestCase
 from django.contrib.auth.models import User
-from views import register
+from .views import register
 
 
 class UserRegistrationTestCase(TestCase):
@@ -13,8 +13,8 @@ class UserRegistrationTestCase(TestCase):
             'last_name': 'Doe',
             'username': 'johndoe',
             'email': 'johndoe@example.com',
-            'password': 's3cret_password',
-            'confirm_password': 's3cret_password',
+            'password1': 'Television1234@',
+            'password2': 'Television1234@',
         }
         response = self.client.post(reverse('register'), form_data)
 
@@ -23,3 +23,72 @@ class UserRegistrationTestCase(TestCase):
         user_exists = User.objects.filter(username='johndoe').exists()
         self.assertTrue(user_exists)
 
+    def test_user_registration_missing(self):
+        form_data = {
+            'first_name': 'John2',
+            'last_name': 'Doe',
+            'username': 'john2doe',
+            'email': 'johnd2oe@example.com',
+        }
+        response = self.client.post(reverse('register'), form_data)
+
+        self.assertEqual(response.status_code, 200)
+        user_exists = User.objects.filter(username='johndoe').exists()
+        self.assertFalse(user_exists)
+
+    def test_user_registration_dup_username(self):
+        form_data1 = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'johndoe@example.com',
+            'password1': 'Television1234@',
+            'password2': 'Television1234@',
+        }
+
+        form_data2 = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'john3doe@example.com',
+            'password1': 'Television1234@',
+            'password2': 'Television1234@',
+        }
+        response = self.client.post(reverse('register'), form_data1)
+
+        self.assertEqual(response.status_code, 302)
+
+        response2 = self.client.post(reverse('register'), form_data2)
+
+        self.assertEqual(response2.status_code, 200)
+
+    def test_user_registration_dup_email(self):
+        form_data1 = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'johndoe@example.com',
+            'password1': 'Television1234@',
+            'password2': 'Television1234@',
+        }
+        form_data2 = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'john2doe',
+            'email': 'johndoe@example.com',
+            'password1': 'Television1234@',
+            'password2': 'Television1234@',
+        }
+        response = self.client.post(reverse('register'), form_data1)
+
+        self.assertEqual(response.status_code, 302)
+
+        response2 = self.client.post(reverse('register'), form_data2)
+
+        self.assertEqual(response2.status_code, 302)
+
+        user_exists = User.objects.filter(username='johndoe').exists()
+        self.assertTrue(user_exists)
+
+        user_exists = User.objects.filter(username='john2doe').exists()
+        self.assertTrue(user_exists)
